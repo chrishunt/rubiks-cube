ecad7311c27846000c1f6fa2dd130f1a29d01de4
Chris Hunt <c@chrishunt.co>
2013-05-13 17:27:46 -0700

    Add Cube with l, r, f, b, d, u operations

---
+++ lib/cube_solver/cube.rb
@@ -0,0 +1,94 @@
+require 'cube_solver/cubie'
+
+module CubeSolver
+  class Cube
+    attr_reader :state
+
+    def initialize(state)
+      @state = build_state_from_string state
+    end
+
+    def state
+      @state.map(&:state).flatten.join ' '
+    end
+
+    def perform!(algorithm)
+      algorithm.split.each do |move|
+        operation = "#{move[0].downcase}!"
+
+        case modifier = move[-1]
+          when "'"
+            3.times { send operation }
+          when "2"
+            2.times { send operation }
+          else
+            send operation
+        end
+      end
+    end
+
+    def r!
+      turn [1, 5, 9, 6]
+      turn [13, 17, 18, 14]
+      rotate [13, 13, 14, 17, 18, 18]
+      self
+    end
+
+    def l!
+      turn [3, 7, 11, 4]
+      turn [12, 15, 19, 16]
+      rotate [12, 15, 15, 16, 16, 19]
+      self
+    end
+
+    def u!
+      turn [0, 1, 2, 3]
+      turn [12, 13, 14, 15]
+      self
+    end
+
+    def d!
+      turn [8, 11, 10, 9]
+      turn [16, 19, 18, 17]
+      self
+    end
+
+    def f!
+      turn [0, 4, 8, 5]
+      rotate [0, 4, 8, 5]
+      turn [12, 16, 17, 13]
+      rotate [12, 12, 13, 16, 17, 17]
+      self
+    end
+
+    def b!
+      turn [2, 6, 10, 7]
+      rotate [2, 6, 10, 7]
+      turn [14, 18, 19, 15]
+      rotate [14, 14, 15, 18, 19, 19]
+      self
+    end
+
+    private
+
+    def build_state_from_string(state)
+      state.split.map { |state| CubeSolver::Cubie.new state }
+    end
+
+    def turn(sequence)
+      current_cubie = sequence.shift
+      first_cubie = @state[current_cubie]
+
+      sequence.each do |cubie|
+        @state[current_cubie] = @state[cubie]
+        current_cubie = cubie
+      end
+
+      @state[current_cubie] = first_cubie
+    end
+
+    def rotate(cubies)
+      cubies.each { |cubie| @state[cubie].rotate! }
+    end
+  end
+end
---
+++ spec/cube_solver/cube_spec.rb
@@ -0,0 +1,85 @@
+require 'cube_solver/cube'
+
+describe CubeSolver::Cube do
+  subject { described_class.new state }
+
+  let(:state) { solved_state }
+
+  let(:solved_state) {
+    'UF UR UB UL FL FR BR BL DF DR DB DL UFL URF UBR ULB DLF DFR DRB DBL'
+  }
+
+  it 'is initialized with a state' do
+    expect(subject.state).to eq state
+  end
+
+  describe '#perform!' do
+    it 'performs the algorithm on the cube' do
+      subject.perform! "U2 D' L R F B"
+
+      expect(subject.state).to eq(
+        described_class.new(solved_state).u!.u!.d!.d!.d!.l!.r!.f!.b!.state
+      )
+    end
+  end
+
+  describe '#r!' do
+    it 'turns the right face 90 degrees clockwise' do
+      subject.r!
+
+      expect(subject.state).to eq(
+        'UF FR UB UL FL DR UR BL DF BR DB DL UFL FRD FUR ULB DLF BDR BRU DBL'
+      )
+    end
+  end
+
+  describe '#l!' do
+    it 'turns the left face 90 degrees clockwise' do
+      subject.l!
+
+      expect(subject.state).to eq(
+        'UF UR UB BL UL FR BR DL DF DR DB FL BUL URF UBR BLD FLU DFR DRB FDL'
+      )
+    end
+  end
+
+  describe '#u!' do
+    it 'turns the up face 90 degrees clockwise' do
+      subject.u!
+
+      expect(subject.state).to eq(
+        'UR UB UL UF FL FR BR BL DF DR DB DL URF UBR ULB UFL DLF DFR DRB DBL'
+      )
+    end
+  end
+
+  describe '#d!' do
+    it 'turns the down face 90 degrees clockwise' do
+      subject.d!
+
+      expect(subject.state).to eq(
+        'UF UR UB UL FL FR BR BL DL DF DR DB UFL URF UBR ULB DBL DLF DFR DRB'
+      )
+    end
+  end
+
+  describe '#f!' do
+    it 'turns the front face 90 degrees clockwise' do
+      subject.f!
+
+      expect(subject.state).to eq(
+        'LF UR UB UL FD FU BR BL RF DR DB DL LFD LUF UBR ULB RDF RFU DRB DBL'
+      )
+    end
+  end
+
+  describe '#b!' do
+    it 'turns the back face 90 degrees clockwise' do
+      subject.b!
+
+      expect(subject.state).to eq(
+        'UF UR RB UL FL FR BD BU DF DR LB DL UFL URF RBD RUB DLF DFR LDB LBU'
+      )
+    end
+  end
+end
