08db553ca2e4e667b937ddcdd53b0093241c7588
Chris Hunt <c@chrishunt.co>
2013-05-15 11:23:17 -0700

    Add Algorithms module with T and Y PLLs

---
+++ lib/cube_solver/algorithms.rb
@@ -0,0 +1,8 @@
+module CubeSolver
+  module Algorithms
+    module PLL
+      T = "R U R' U' R' F R2 U' R' U' R U R' F'"
+      Y = "U F R U' R' U' R U R' F' R U R' U' R' F R F' U'"
+    end
+  end
+end
